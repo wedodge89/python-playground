017c0de2d9054b9139a773acf5a915b30d47a7d9
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-08-03 14:43:31 -0500

    did augmented assignment practice

---
+++ 1basics/practice-augmented-assignment.py
@@ -0,0 +1,19 @@
+counter = 0
+# 0
+
+counter += 1
+# 1
+counter += 1
+# 2
+counter += 1
+# 3
+counter += 1
+# 4
+counter -= 1
+# 3
+counter *=2
+# 6
+# Correct!
+
+#Before you click RUN, guess what the counter variable holds in memory!
+print(counter)
