7859989d6998479f6fdffec4298361172a7c1008
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-08-04 14:37:42 -0500

    practiced string indexing

--- 1basics/text.py
+++ 1basics/text.py
@@ -34,3 +34,18 @@ print("Hi, {}. You are {} years old.".format(name, age))
 print("Hi, {1}. You are {0} years old.".format(age, name))
 
 # string indexes
+alphabet = "abcdefghij"
+          # 0123456789
+
+# [start:stop:stepover]
+print(alphabet)
+print(alphabet[0])
+print(alphabet[1])
+print(alphabet[0:2])
+print(alphabet[3:5])
+print(alphabet[0:10])
+print(alphabet[0:10:2])
+print(alphabet[-1])
+print(alphabet[-2])
+print(alphabet[::-1])
+print(alphabet[::-2])
