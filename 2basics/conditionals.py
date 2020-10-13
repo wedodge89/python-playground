874a17c042da37d777d5bf5587d9987212a535c4
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2020-10-13 14:38:45 -0500

    added alternative approach to conditionals

--- 2basics/conditionals.py
+++ 2basics/conditionals.py
@@ -11,4 +11,17 @@ if is_old:
         print("You may be old enough, but you aren't licensed.")
 elif is_licensed:
     print("How did you get a license, you whippersnapper?")
+else:
+    print("No dice, bro.")
+
+# Alternative approach:
+if is_old and is_licensed:
+    print("You can drive.")
+elif is_old and not is_licensed:
+    print("Age ain't everything.")
+elif is_licensed and not is_old:
+    print("That must be a fake license.")
+else:
+    print("You need to grow up and get a license.")
+
 
