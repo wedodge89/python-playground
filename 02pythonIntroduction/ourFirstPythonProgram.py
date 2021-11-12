a4953873480dd24ad7ac3b45130c083854dd3e4a
wedodge89 <elliott.dodge1@gmail.com>
2021-11-12 15:55:37 -0600

    adding directories and doc file

---
+++ 02pythonIntroduction/ourFirstPythonProgram.py
@@ -0,0 +1,12 @@
+# print first name only
+print("Elliott")
+
+# print first and last name (concatenation)
+print("Elliott " + "Dodge")
+
+# print input
+name = input("What is your name? ")
+print(name)
+
+# print string + input (concatenated)
+print("Hello, " + name)
