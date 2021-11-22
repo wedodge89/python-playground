7f3eef80b8f8b8a11806ab2bc33ff22e10650c0e
wedodge89 <elliott.dodge1@gmail.com>
2021-11-22 16:39:18 -0600

    through lesson 48/332

---
+++ 03_python_basics/47_common_list_patterns.py
@@ -0,0 +1,10 @@
+basket = ["a", "x", "b", "c", "d", "e", "d"]
+basket.sort()
+basket.reverse()
+print(basket[::-1])
+print(basket)
+
+print(list(range(100)))
+
+sentence = " "
+print(sentence.join(["Hi", "my", "name", "is", "Elliott."]))
