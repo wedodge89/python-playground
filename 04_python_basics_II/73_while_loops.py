a2a1e1c49790d425c3ec26acfce098f9bd444931
wedodge89 <elliott.dodge1@gmail.com>
2021-12-13 16:22:35 -0600

    through lesson 76/332

---
+++ 04_python_basics_II/73_while_loops.py
@@ -0,0 +1,6 @@
+i = 0
+while i < 50:
+    print(i)
+    i += 1
+else:
+    print("done with all the work")
---
+++ 04_python_basics_II/75_break_continue_pass.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3]
+
+for item in my_list:
+    continue
+    print(item)
+
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    pass
+    
